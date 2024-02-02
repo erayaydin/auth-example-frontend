37a15d70a3cbde3ba3c4ada062cdd2cd7bc78031
Eray Aydın <erayaydinn@protonmail.com>
2024-02-02 19:41:25 +0300

    feat: add eslint support

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: [
+        'eslint:recommended'
+    ],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json', './tsconfig.node.json'],
+        tsconfigRootDir: __dirname,
+    },
+    plugins: [],
+    settings: {
+        react: {
+            version: '18.2'
+        }
+    }
+}
